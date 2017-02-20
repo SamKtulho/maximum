25dc63953ea4d70821d6d28d9b02b80b3a3e33a2
samktulho <algerman.sam@gmail.com>
2017-02-20 23:28:42 +0700

    fix emails

--- app/Services/Random.php
+++ app/Services/Random.php
@@ -11,7 +11,7 @@ class Random
     private static $masks = [
         'mail' => ['@mail.ru', '@list.ru', '@bk.ru', '@inbox.ru'],
         'yandex' => ['@yandex.%'],
-        'gmail' => ['@gmail.%']
+        'gmail' => ['@gmail.%'],
     ];
     public static function prepareData($text, $title, $edomain, $tic)
     {
@@ -19,11 +19,26 @@ class Random
         foreach ($edomain as $domain) {
             if (isset(self::$masks[$domain])) {
                 foreach (self::$masks[$domain] as $pat)
-                $pattern .= 'LIKE \'%' . $pat . '\' OR e.email ';
-                break;
+                    $pattern .= 'LIKE \'%' . $pat . '\' OR e.email ';
+            }
+            if ($domain === 'other') {
+                $need = false;
+                if (!empty($pattern)) {
+                    $pattern = substr($pattern, 0, -12);
+                    $pattern .= ' OR (e.email ';
+                    $need = true;
+                }
+                foreach (self::$masks as $pat)
+                    foreach ($pat as $p) {
+                        $pattern .= 'NOT LIKE \'%' . $p . '\' AND e.email ';
+                    }
+                $pattern = substr($pattern, 0, -13);
+                if ($need) $pattern .= ')';
+
             }
         }
-        if (!empty($pattern)) $pattern = substr($pattern, 0, -12);
+        if (!empty($pattern) && !in_array('other', $edomain)) $pattern = substr($pattern, 0, -12);
+
             $results = DB::select('SELECT d.status, e.email, d.domain from domains as d RIGHT JOIN emails as e ON d.id = e.domain_id WHERE e.is_valid = 1 AND d.tic ' . ($tic > 1 ? ' = ' : ' > ') . ' ? AND d.status = 0 AND (e.email ' . $pattern . ');', [$tic]);
         $results = reset($results);
 
