858ef69180ddc8fa3ae124a227a58db2e7b279bb
samktulho <algerman.sam@gmail.com>
2017-02-22 23:28:14 +0700

    statistic for email

--- app/Models/Domain.php
+++ app/Models/Domain.php
@@ -13,12 +13,15 @@ class Domain extends Model
     public function prepareData($content)
     {
         if (empty($content)) return null;
-
-
     }
 
     public function emails()
     {
         return $this->hasMany('App\Models\Email');
+    }
+
+    public function shorturls()
+    {
+        return $this->hasMany('\App\Models\Shorturl');
     }
 }
---
+++ resources/views/email/statistic.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/jq-2.2.4/dt-1.10.13/datatables.min.css"/>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('//cdn.datatables.net/v/bs/jq-2.2.4/dt-1.10.13/datatables.min.js') }}"></script>
+
+    <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+        <thead>
+        <tr>
+            <th>Ссылка</th>
+            <th>Домен</th>
+            <th>Создатель</th>
+            <th>Дата</th>
+            <th>Клики</th>
+        </tr>
+        </thead>
+        <tfoot>
+        <tr>
+            <th>Ссылка</th>
+            <th>Домен</th>
+            <th>Создатель</th>
+            <th>Дата</th>
+            <th>Клики</th>
+        </tr>
+        </tfoot>
+        <tbody>
+        @foreach ($shortUrls as $shortUrl)
+            <tr>
+                <td><a href="{{ $shortUrl->url }}">{{ $shortUrl->url }} </a></td>
+                <td> {{ $shortUrl->domain->domain }} </td>
+                <td> {{ $shortUrl->user->name }} </td>
+                <td> {{ $shortUrl->created_at }} </td>
+                <td> {{ 0 }} </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable();
+        } );
+    </script>
+@endsection
