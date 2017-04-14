7ce9347fc7e56a6420f4bcd53943163a99c7746d
samktulho <algerman.sam@gmail.com>
2017-04-14 19:01:16 +0700

    domains to subdomains command

--- app/Console/Commands/AddDomainType.php
+++ app/Console/Commands/AddDomainType.php
@@ -7,6 +7,14 @@ use App\Models\Domain;
 
 class AddDomainType extends Command
 {
+
+    private $subdomainToExclude = [
+        '\.com\.ru',
+        '\.nov\.ru',
+        '\.net\.ru',
+        '\.org\.ru',
+    ];
+
     /**
      * The name and signature of the console command.
      *
@@ -38,11 +46,22 @@ class AddDomainType extends Command
      */
     public function handle()
     {
-        Domain::chunk(200, function ($domains) {
+        $subdomainToExcludePattern = '~';
+        foreach ($this->subdomainToExclude as $id => $pattern) {
+            $subdomainToExcludePattern .= $id ? '|(' . $pattern . ')' : '(' . $pattern . ')';
+        }
+        $subdomainToExcludePattern .= '~';
+
+
+        Domain::chunk(200, function ($domains) use ($subdomainToExcludePattern) {
             foreach ($domains as $domain) {
-                if ($domain->type == Domain::TYPE_EMAIL && $domain->links->first()) {
-                    $domain->type = Domain::TYPE_LINK;
+                if ($domain->type != Domain::TYPE_SUBDOMAIN
+                    && substr_count($domain->domain, '.') > 1
+                    &&  !preg_match($subdomainToExcludePattern, $domain->domain)
+                ) {
+                    $domain->type = Domain::TYPE_SUBDOMAIN;
                     $domain->save();
+                    $this->info($domain->domain);
                 }
             }
         });
