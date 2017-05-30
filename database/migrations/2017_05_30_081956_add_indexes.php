ebc86840f177d49b1ea96f21d81ffa4754dd852d
Sam <sam@coccoc.com>
2017-05-30 12:29:12 +0700

    add indexes

---
+++ database/migrations/2017_05_30_081956_add_indexes.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIndexes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('emails', function ($table) {
+            $table->index('is_valid');
+        });
+
+        Schema::create('domains', function (Blueprint $table) {
+            $table->index(['status', 'type']);
+        });
+
+        Schema::create('links', function (Blueprint $table) {
+            $table->index(['status', 'registrar']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
