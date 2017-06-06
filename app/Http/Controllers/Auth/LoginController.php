f3a4f8149a2400f244dd6f572806e5385538b228
samktulho <algerman.sam@gmail.com>
2017-06-07 00:22:25 +0700

    change redirect page

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/random/email';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
