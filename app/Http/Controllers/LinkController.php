bba1b4799d57d0c85baca9ef99291a5a15481913
German <sam@coccoc.com>
2017-07-10 18:05:55 +0700

    link stat only good actions

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -71,7 +71,7 @@ class LinkController extends Controller
     
     public function data(Datatables $datatables)
     {
-        $query = Shorturl::with('user')->with('domain')->with('urlstats')->select('shorturls.*')->where('type', Shorturl::TYPE_REGISTRAR);
+        $query = Shorturl::with('user')->with('domain')->with('urlstats')->select('shorturls.*')->where('type', Shorturl::TYPE_REGISTRAR)->whereIn('action', [Shorturl::ACTION_FORM_SENT, Shorturl::ACTION_MAIL_SENT]);
 
         return $datatables->eloquent($query)
             ->addColumn('stat', function ($shorturl) {
